62df6d5ae7a496157412870cfc48608108df2731
idarciooliveira <idarciooliveira@gmail.com>
2022-02-15 02:19:18 -0800

    update sidebar

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,22 +1,37 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
 import styles from './styles.module.scss';
+import { FiInbox, FiPackage, FiSettings, FiUser } from 'react-icons/fi'
 
 const SideBar: NextPage = ()=>{
     return (
     <div className={styles.sidebar}>
-       <Link href={'/'}>
-            <a>MX Delivery</a>
-       </Link>
-        <Link href={'/orders'}>
-            <a>Pedidos</a>
-        </Link>
-        <Link href={'/products'}>
-            <a>Produtos</a>
-        </Link>
-        <Link href={'/custumers'}>
-            <a>Clientes</a>
-        </Link>
+       <nav>
+            <Link href={'/'}>
+                    <a>MX Delivery</a>
+            </Link>
+            <Link href={'/orders'}>
+                <a>
+                    <i><FiInbox size={24}/></i>
+                    Pedidos
+                </a>
+            </Link>
+            <Link href={'/custumers'}>
+                <a>
+                    <i><FiUser size={24}/></i>
+                    Clientes
+                </a>
+            </Link>
+            <Link href={'/products'}>
+                <a>
+                    <i><FiPackage size={24}/></i>
+                    Produtos
+                </a>
+            </Link>
+       </nav>
+        
+        <h3>POWERED BY OKUTECH</h3>
+
       </div>
     )
 }
